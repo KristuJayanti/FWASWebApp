909c9f0ed05c2242a6d6b07ceb280bf4f6f76207
Nikhil Hari <nikhil_hari@live.com>
2020-06-02 17:43:16 +0530

    Use port 8080 for now to prevent conflict with API server

--- TGenWebApp/Program.cs
+++ TGenWebApp/Program.cs
@@ -20,9 +20,9 @@ namespace TGenWebApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://*:8080");
                 });
     }
 }
