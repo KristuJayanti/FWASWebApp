97e5c1fa21be3ed0a60c9fcc97822abff30d0cb6
Nikhil Hari <nikhil_hari@live.com>
2020-06-05 18:21:32 +0530

    Clean up code
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

---
+++ TGenWebApp/Services/ApiBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace TGenWebApp.Services {
+    internal static class ApiBase {
+        
+        internal const string BaseUrl = "https://localhost:5001/";
+        
+        internal static IRestRequest GenerateRequest(string jsonBody) {
+            return new RestRequest(Method.POST) { RequestFormat = DataFormat.Json}
+                .AddHeader("Access-Control-Allow-Origin", "*")
+                .AddHeader("Accept", "application/json")
+                .AddJsonBody(jsonBody);
+        }
+
+        internal static Dictionary<string, string> GetDict(string response) {
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
+        }
+    }
+}
--- TGenWebApp/Services/ApiService.cs
+++ TGenWebApp/Services/ApiService.cs
@@ -10,36 +10,23 @@ using RestSharp.Serialization.Json;
 
 namespace TGenWebApp.Services {
     public static class ApiService {
-        private const string BaseUrl = "https://localhost:5001/";
-        
-        private static IRestRequest GenerateRequest(string jsonBody) {
-            return new RestRequest(Method.POST) { RequestFormat = DataFormat.Json}
-                .AddHeader("Access-Control-Allow-Origin", "*")
-                .AddHeader("Accept", "application/json")
-                .AddJsonBody(jsonBody);
-        }
-
-        private static Dictionary<string, string> GetDict(string response) {
-            return JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
-        }
-        
         public static async Task<bool> IsValidUsername(string username) {
-            var client = new RestClient($"{BaseUrl}CheckUsername") {Timeout = -1};
-            var request = GenerateRequest($@"{{""username"":""{username}""}}");
+            var client = new RestClient($"{ApiBase.BaseUrl}CheckUsername") {Timeout = -1};
+            var request = ApiBase.GenerateRequest($@"{{""username"":""{username}""}}");
             var response = await client.ExecuteAsync(request);
-            var re = GetDict(response.Content);
+            var re = ApiBase.GetDict(response.Content);
             return bool.Parse(re["isUsernameExists"]);
         }
 
         public static async Task<string> Login(string username, string password) {
-            var client = new RestClient($"{BaseUrl}UserAuth") {Timeout = -1};
-            var request = GenerateRequest($@"{{""username"":""{username}"", 
+            var client = new RestClient($"{ApiBase.BaseUrl}UserAuth") {Timeout = -1};
+            var request = ApiBase.GenerateRequest($@"{{""username"":""{username}"", 
                                                         ""password"":""{password}""}}");
             var response = await client.ExecuteAsync(request);
-            var result = GetDict(response.Content);
+            var result = ApiBase.GetDict(response.Content);
             if (result["validationMessage"] == "Validation Success") {
-                var sess = new Session() {
-                    Id = result["userId"],
+                var sess = new Session {
+                    Id = result["userId"]
                 };
                 return SessionManager.AddSession(sess);
             }
