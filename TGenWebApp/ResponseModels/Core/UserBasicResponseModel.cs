53696e8fcbcd74ece2774c240ddcafb0d7a35aa1
Nikhil Hari <nikhil_hari@live.com>
2020-06-10 21:00:05 +0530

    Import missed InstitutionID
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

--- TGenWebApp/ResponseModels/Core/UserBasicResponseModel.cs
+++ TGenWebApp/ResponseModels/Core/UserBasicResponseModel.cs
@@ -1,11 +1,12 @@
 ﻿namespace TGenWebApp.ResponseModels.Core {
     public class UserBasicResponseModel {
+        public string institutionId { get; set; }
         public string name { get; set; }
         public string emailId { get; set; }
         public string photo { get; set; }
         public long phoneNumber { get; set; }
         public string address { get; set; }
-        public Address addressLocation { get; set; }
+        public Address addressLocation { get; set; } = new Address();
         public int pincode { get; set; }
         public bool initialSetup { get; set; }
     }
