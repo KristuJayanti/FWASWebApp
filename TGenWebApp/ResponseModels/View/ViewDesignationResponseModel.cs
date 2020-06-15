8ef914103836d0e7980f7352b7ec80ed81312b16
Nikhil Hari <nikhil_hari@live.com>
2020-06-15 05:50:54 +0530

    Import LibraryNlightzCollegeRequestResponseModel.ResponseModel.CollegeInitialSetupResponseModel.View.ViewDesignationResponseModel
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

---
+++ TGenWebApp/ResponseModels/View/ViewDesignationResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TGenWebApp.ResponseModels.View {
+    public class ViewDesignationResponseModel {
+        public List<Designations> designations { get; set; } = new List<Designations>();
+    }
+
+    public class Designations {
+        public string title { get; set; }
+        public List<AccessCode> codes { get; set; } = new List<AccessCode>();
+        public string id { get; set; }
+    }
+
+    public class AccessCode {
+        public int code { get; set; }
+        public string name { get; set; }
+    }
+}
