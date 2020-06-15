82c183f8bc06586443b8c960a445fee1d7ad104c
Nikhil Hari <nikhil_hari@live.com>
2020-06-15 10:05:53 +0530

    Pull Building and Room Response
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

---
+++ TGenWebApp/ResponseModels/Manager/CollegeInfrastructureBuildingResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TGenWebApp.ResponseModels.Manager {
+    public class CollegeInfrastructureBuildingResponseModel {
+        public string id { get; set; }
+        public string buildingCode { get; set; }
+        public string buildingName { get; set; }
+        public int numberOfFloors { get; set; }
+    }
+}
---
+++ TGenWebApp/ResponseModels/Manager/CollegeInfrastructureRoomResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TGenWebApp.ResponseModels.Manager {
+    public class CollegeInfrastructureRoomResponseModel {
+        public string id { get; set; }
+        public string buildingId { get; set; }
+        public string buildingCode { get; set; }
+        public int floorNumber { get; set; }
+        public string roomCode { get; set; }
+        public bool IsLab { get; set; }
+        public string labType { get; set; }
+        public string roomType { get; set; }
+        public int maxStrength { get; set; }
+        public bool IsUG { get; set; }
+        public bool IsPG { get; set; }
+    }
+}
