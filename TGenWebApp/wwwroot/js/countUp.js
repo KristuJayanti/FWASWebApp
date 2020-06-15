d20bb04b515ec3d94c7dc915e719196c89940793
Nikhil Hari <nikhil_hari@live.com>
2020-06-15 05:52:45 +0530

    Count-up animation [1]
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

---
+++ TGenWebApp/wwwroot/js/countUp.js
@@ -0,0 +1,18 @@
+﻿window.countUp = () => {
+    $('.counter').each(function () {
+        let $this = $(this),
+            countTo = $this.attr('data-count');
+        $({countNum: $this.text()})
+            .animate({
+                countNum: countTo
+            },{
+                duration: 500,
+                easing: 'swing',
+                step: () =>  {
+                    $this.text(Math.floor(this.countNum));
+                }, complete: () => {
+                    $this.text(this.countNum);
+                }
+            });
+    });
+};
