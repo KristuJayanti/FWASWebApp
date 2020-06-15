9840d20a248453ed5953982da2246ccbb9072ff3
Nikhil Hari <nikhil_hari@live.com>
2020-06-15 06:05:26 +0530

    Introduce View and Manage API clients
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

--- TGenWebApp/Services/ViewApi.cs
+++ TGenWebApp/Services/ViewApi.cs
@@ -1,5 +1,22 @@
-﻿namespace TGenWebApp.Services {
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using TGenWebApp.ResponseModels.View;
+
+namespace TGenWebApp.Services {
     public static class ViewApi {
-        
+        public static async Task<ViewDesignationResponseModel> ViewDesignation(string institutionId) {
+            await Logger.Log($"Called /ViewDesignation for {institutionId}", LogMode.Info);
+            var client = new RestClient($"{Constants.BaseUrl}ViewDesignation")  {
+                Timeout = -1,
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+            };
+            var request = ApiBase.GenerateRequest($@"{{""institutionID"":""{institutionId}""}}");
+            var response = await client.ExecuteAsync(request);
+            if (response.IsSuccessful)
+                return JsonConvert.DeserializeObject<ViewDesignationResponseModel>(response.Content);
+            await Logger.Log($"API Server failed when getting designations for {institutionId}.", LogMode.Error);
+            return null;
+        }
     }
 }
