591efd4c8ccf9baa1eded6b17ffc3d03c433fa5d
Nikhil Hari <nikhil_hari@live.com>
2020-06-20 10:11:09 +0530

    Import ViewCollegeConfigResponseModel from NlightzResponseRequestModel
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

---
+++ TGenWebApp/ResponseModels/View/ViewCollegeConfigResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TGenWebApp.ResponseModels.View {
+    public class ViewCollegeConfigResponseModel
+    {
+        public bool IsUG { get; set; }
+        public bool IsPG { get; set; }
+        public List<Designations> designations { get; set; } = new List<Designations>();
+        public List<AccessCode> accessCodes { get; set; } = new List<AccessCode>();
+    }
+}
