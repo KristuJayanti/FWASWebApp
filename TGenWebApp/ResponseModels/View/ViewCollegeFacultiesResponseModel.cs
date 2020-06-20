2f12c3c24527de1cdea042d6aef60a1ece0685f5
Nikhil Hari <nikhil_hari@live.com>
2020-06-20 13:34:40 +0530

    Import ViewCollegeFacultiesResponseModel from ResponseModel
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

---
+++ TGenWebApp/ResponseModels/View/ViewCollegeFacultiesResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TGenWebApp.ResponseModels.View {
+    public class ViewCollegeFacultiesResponseModel {
+        public string userId { get; set; }
+        public string name { get; set; }
+        public string jobId { get; set; }
+    }
+}
