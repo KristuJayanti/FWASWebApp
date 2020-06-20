fa36db234e0f629a7afc7900910530ad9f4a5c09
Nikhil Hari <nikhil_hari@live.com>
2020-06-20 14:31:49 +0530

    Institution details caching [1]
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

---
+++ TGenWebApp/Services/Institution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using TGenWebApp.ResponseModels.View;
+
+namespace TGenWebApp.Services {
+    public class Institution {
+
+        private ViewCollegeConfigResponseModel _config;
+        private List<ViewCollegeFacultiesResponseModel> _faculties;
+        
+        public string InstitutionId { get; }
+        public string InstitutionName { get; }
+
+        public Institution(string institutionId, string institutionName) {
+            InstitutionId = institutionId;
+            InstitutionName = institutionName;
+        }
+
+        // /!\ BLOCKING
+        public ViewCollegeConfigResponseModel CollegeConfig {
+            get { return _config ??= ViewApi.ViewCollegeConfig(InstitutionId).Result; }
+        }
+        
+        // /!\ BLOCKING
+        public List<ViewCollegeFacultiesResponseModel> CollegeFaculties {
+            get { return _faculties ??= ViewApi.ViewCollegeFaculties(InstitutionId).Result; }
+        }
+    }
+}
---
+++ TGenWebApp/Services/InstitutionManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TGenWebApp.Services {
+    public static class InstitutionManager {
+        private static readonly Dictionary<string, Institution> Institutions  = new Dictionary<string, Institution>();
+
+        public static void AddInstitution(string institutionId, string institutionName) {
+            if (!Institutions.ContainsKey(institutionId)) {
+                Institutions.Add(institutionId, new Institution(institutionId, institutionName));
+            }
+        }
+
+        public static Institution GetInstitution(string institutionId) {
+            return Institutions.ContainsKey(institutionId) ? Institutions[institutionId] : null;
+        }
+        
+        // TODO: implement automatic worker to remove from cache
+    }
+}
