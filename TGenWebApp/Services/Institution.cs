4022c517209992cccbd90d1461a88f37b099cd43
Nikhil Hari <nikhil_hari@live.com>
2020-06-22 01:09:40 +0530

    Add Cache Reset methods: Institution Caching [3]
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

--- TGenWebApp/Services/Institution.cs
+++ TGenWebApp/Services/Institution.cs
@@ -22,17 +22,33 @@ namespace TGenWebApp.Services {
         public async Task<ViewCollegeConfigResponseModel> GetCollegeConfigResponse() {
            return _config ??= await ViewApi.ViewCollegeConfig(InstitutionId);
         }
+
+        public void ResetCollegeConfigResponse() {
+            _config = null;
+        }
         
         public async Task<List<ViewCollegeFacultiesResponseModel>> GetCollegeFaculties() {
             return _faculties ??= await ViewApi.ViewCollegeFaculties(InstitutionId);
+        }
+
+        public void ResetCollegeFaculties() {
+            _faculties = null;
         }
 
         public async Task<List<CollegeInfrastructureBuildingResponseModel>> GetCollegeBuildings() {
             return _buildings ??= await ViewApi.ViewCollegeBuildings(InstitutionId);
         }
 
+        public void ResetCollegeBuildings() {
+            _buildings = null;
+        }
+
         public async Task<List<CollegeInfrastructureRoomResponseModel>> GetCollegeRooms() {
             return _rooms ??= await ViewApi.ViewCollegeRooms(InstitutionId);
+        }
+
+        public void ResetCollegeRooms() {
+            _rooms = null;
         }
     }
 }
