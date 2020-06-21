1758ef4075178dcff6fbb38d19c2b8db915bd3a8
Nikhil Hari <nikhil_hari@live.com>
2020-06-21 22:07:47 +0530

    Implement the rest of View APIs.

--- TGenWebApp/Services/ViewApi.cs
+++ TGenWebApp/Services/ViewApi.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RestSharp;
+using TGenWebApp.ResponseModels.Manager;
 using TGenWebApp.ResponseModels.View;
 
 namespace TGenWebApp.Services {
@@ -43,10 +44,15 @@ namespace TGenWebApp.Services {
             Logger.Log($"API Server failed when getting College Config for {institutionId}.", LogMode.Error);
             return null;
         }
-
+        
+        /// <summary>
+        /// Get Faculty details. Endpoint for /ViewCollegeFaculties
+        /// </summary>
+        /// <param name="institutionId">Institution ID</param>
+        /// <returns>List of Faculties</returns>
         public static async Task<List<ViewCollegeFacultiesResponseModel>> ViewCollegeFaculties(string institutionId) {
             Logger.Log($"Called /ViewCollegeFaculties for {institutionId}", LogMode.Info);
-            var client = new RestClient($"{Constants.BaseUrl}ViewCollegeFaculties")  {
+            var client = new RestClient($"{Constants.BaseUrl}ViewCollegeFaculties") {
                 Timeout = -1,
                 RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
             };
@@ -55,6 +61,47 @@ namespace TGenWebApp.Services {
             if (response.IsSuccessful)
                 return JsonConvert.DeserializeObject<List<ViewCollegeFacultiesResponseModel>>(response.Content);
             Logger.Log($"API Server failed when getting College Faculties for {institutionId}.", LogMode.Error);
+            return null;
+        }
+        
+        /// <summary>
+        /// Get a list of buildings available in the institution.
+        /// </summary>
+        /// <param name="institutionId">Institution ID</param>
+        /// <returns>A list with all the buildings.</returns>
+        public static async Task<List<CollegeInfrastructureBuildingResponseModel>>
+            ViewCollegeBuildings(string institutionId) {
+            Logger.Log($"Called /ViewCollegeInfrastructure:Building for {institutionId}", LogMode.Info);
+            var client = new RestClient($"{Constants.BaseUrl}ViewCollegeInfrastructure") {
+                Timeout = -1,
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+            };
+            var request = ApiBase
+                .GenerateRequest($@"{{""institutionID"":""{institutionId}""}}, {{""isRoom"":""false""}}");
+            var response = await client.ExecuteAsync(request);
+            if (response.IsSuccessful)
+                return JsonConvert.DeserializeObject<List<CollegeInfrastructureBuildingResponseModel>>(response.Content);
+            Logger.Log($"API Server failed when getting College Infrastructure for {institutionId}.", LogMode.Error);
+            return null;
+        }
+
+        /// <summary>
+        /// Get a list of rooms available in the institution.
+        /// </summary>
+        /// <param name="institutionId">Institution ID</param>
+        /// <returns>A list containing all the rooms.</returns>
+        public static async Task<List<CollegeInfrastructureRoomResponseModel>> ViewCollegeRooms(string institutionId) {
+            Logger.Log($"Called /ViewCollegeInfrastructure:Building for {institutionId}", LogMode.Info);
+            var client = new RestClient($"{Constants.BaseUrl}ViewCollegeInfrastructure") {
+                Timeout = -1,
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+            };
+            var request = ApiBase
+                .GenerateRequest($@"{{""institutionID"":""{institutionId}""}}, {{""isRoom"":""true""}}");
+            var response = await client.ExecuteAsync(request);
+            if (response.IsSuccessful)
+                return JsonConvert.DeserializeObject<List<CollegeInfrastructureRoomResponseModel>>(response.Content);
+            Logger.Log($"API Server failed when getting College Infrastructure for {institutionId}.", LogMode.Error);
             return null;
         }
     }
