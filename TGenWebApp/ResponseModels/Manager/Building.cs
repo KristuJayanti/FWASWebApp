2d7e1b9c55a46dff82be19ed3deb6f044335fd98
Nikhil Hari <nikhil_hari@live.com>
2020-06-28 02:00:24 +0530

    CollegeInfrastructureBuildingResponseModel to Building.
    Signed-off-by: Nikhil Hari <nikhil_hari@live.com>

--- TGenWebApp/ResponseModels/Manager/Building.cs
+++ TGenWebApp/ResponseModels/Manager/Building.cs
@@ -1,8 +1,10 @@
 ﻿namespace TGenWebApp.ResponseModels.Manager {
-    public class CollegeInfrastructureBuildingResponseModel {
+    public class Building {
         public string id { get; set; }
         public string buildingCode { get; set; }
         public string buildingName { get; set; }
         public int numberOfFloors { get; set; }
     }
+    
+    
 }
